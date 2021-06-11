a4f4b263e85483d3f8cd2a95e8d0f0fa921fea3f
stef7 <1480673+stef7@users.noreply.github.com>
2021-06-11 20:20:16 +1000

    Node running alone

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Nextcraft</title>
+			</Head>
+
+			<main id="content" role="main">
+				<h1>Nextcraft</h1>
+			</main>
+		</>
+	);
+}
